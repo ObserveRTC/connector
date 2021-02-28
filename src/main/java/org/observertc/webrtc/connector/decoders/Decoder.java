5f561d66dbebd76b86b171970cec352f745236ff
Balazs Kreith <kreith.balazs@gmail.com>
2021-02-28 21:29:08 +0200

    purge contents for the pranch

--- src/main/java/org/observertc/webrtc/connector/decoders/Decoder.java
+++
@@ -1,8 +0,0 @@
-package org.observertc.webrtc.connector.decoders;
-
-import io.reactivex.rxjava3.core.ObservableOperator;
-import org.observertc.webrtc.schemas.reports.Report;
-
-public interface Decoder extends ObservableOperator<Report, byte[]> {
-
-}
